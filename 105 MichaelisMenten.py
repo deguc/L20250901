0a79d23d6e3b85c3d2b4585a98ed3d5a9ac891a1
deguc <113810267+deguc@users.noreply.github.com>
2025-09-14 17:09:26 +0900

    Update 105 MichaelisMenten.py

--- 105 MichaelisMenten.py
+++ 105 MichaelisMenten.py
@@ -1,9 +1,9 @@
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
 
-def michaelis_menten(var,t,*args):
+def michaelis_menten(t,var,*args):
 
     S,ES,P = var
     kon,koff,k,E0 = args
@@ -19,20 +19,29 @@ def michaelis_menten(var,t,*args):
 S0,E0 = 1000,1
 kon,koff,k = 1,5,0.1
 
-init = (S0,0,0)
+init = np.array([S0,0,0])
 
-t = np.linspace(0,13000,1000)
+t_span=(0,15000)
+t_eval = np.linspace(0,13000,1000)
 
-sol = odeint(michaelis_menten,init,t,args=(kon,koff,k,E0))
+sol = solve_ivp(
+    michaelis_menten,
+    t_span,
+    init,
+    args=(kon,koff,k,E0),
+    method='LSODA',
+    t_eval = t_eval,
+    rtol=5e-4,
+    atol=1e-6
+)
 
-S = sol[:,0]
-ES = sol[:,1]
-P = sol[:,2]
+t = sol.t
+S,ES,P = sol.y
 
 fig = plt.figure(figsize=(6,4))
-plt.plot(t,ES/E0,color='red',label='[ES]/[E0]')
-plt.plot(t,P/S0,color='black',label='[P]/[S0]')
-plt.plot(t,(S+ES)/S0,color='blue',label='([S]+[ES]/[S0]')
+plt.plot(t,ES/E0,color='red',label='[ES]/E0')
+plt.plot(t,P/S0,color='black',label='[P]/S0')
+plt.plot(t,(S+ES)/S0,color='blue',label='([S]+[ES]/S0')
 plt.title('Michaelis-Menten')
 plt.xlabel('t')
 plt.ylabel('Concentration')
